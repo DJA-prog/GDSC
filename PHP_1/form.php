5fcc5b30fad6f27239e1a9c98b2d72962af98913
Dino Jose Almirall <almirall.dino@gmail.com>
2022-03-12 20:18:43 +0200

    php

---
+++ PHP_1/form.php
@@ -0,0 +1,28 @@
+<?php
+  //get values
+  $value1 = $_GET["value1"]; //6
+  $value2 = $_GET["value2"]; //3
+  $value3 = $_GET["opperation"]; // "div"
+
+  // opperations
+  if ($value3 == "add"){
+    $answer = $value1+$value2;
+  }
+  if ($value3 == "sub"){
+    $answer = $value1-$value2;
+  }
+  if ($value3 == "mul"){
+    $answer = $value1*$value2;
+  }
+  if ($value3 == "div"){
+    $answer = $value1/$value2;
+  }
+
+  // print
+  echo "The answer is ".$answer;
+
+  // ==
+  // <
+  // >
+  // !=
+ ?>
---
+++ PHP_1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/master.css">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="form.php" method="get">
+      <input type="number" name="value1" value="">
+      <input type="number" name="value2" value="">
+      <select class="" name="opperation">
+        <option value="add">add</option>
+        <option value="sub">subtract</option>
+        <option value="mul">multiply</option>
+        <option value="div">divide</option>
+      </select>
+      <input type="submit" name="submit" value="Equal">
+    </form>
+  </body>
+</html>
