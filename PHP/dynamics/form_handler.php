2fa03394ac175c3bd064009f8277e4c99338e06e
Dino Jose Almirall <almirall.dino@gmail.com>
2022-04-02 20:09:10 +0200

    Dynamic PHP

--- PHP/dynamics/form_handler.php
+++ PHP/dynamics/form_handler.php
@@ -1,7 +1,41 @@
 <?php
 
 if (isset($_POST['submit_1'])) {
-  
+  $newFileName = $_POST["newfilename"];
+  $file = $_FILES["upload_file"];
+  // print_r($_FILES); // print the array called $file
+  // exit;
+  $fileName = $_FILES["upload_file"]["name"];
+  $fileTmpName = $_FILES["upload_file"]["tmp_name"];
+  $fileSize = $_FILES["upload_file"]["size"];
+  $fileError = $_FILES["upload_file"]["error"];
+  $fileType = $_FILES["upload_file"]["type"];
+
+  $fileExt = pathinfo($fileName, PATHINFO_EXTENSION); // returns the file extention
+  $fileExt = strtolower($fileExt); // make extention lowercase due to some may be uppercase
+  echo $fileExt;
+  $allowedFormats = array("jpg", "jpeg", "png"); // a list of file types allowed to be uploaded
+
+  if (in_array($fileExt, $allowedFormats)) { // check if uploaded file is allowed
+    if ($fileError === 0) { // === is the same as == but will check if it is related
+      if ($fileSize < 2000000) { // allow for only file smaller than 1 000 000 B
+        // $fileNameNew = uniqid('', true).'.'.$fileExt; // get time format in microseconds used for new upload name, create a new file name
+        $fileDestination = "./uploads/".$newFileName.'.'.$fileExt;
+        if (move_uploaded_file($fileTmpName, $fileDestination)) {
+          header("Location: ./index.php?upload=success");
+          exit;
+        }else{
+          echo "Upload failed on server side";
+        }
+      }else{
+        echo "File size limit exceeded";
+      }
+    }else{
+      echo "Upload error during transfer";
+    }
+  }else{
+    echo "Format not permitted";
+  }
 }elseif (isset($_POST['submit_2'])) {
 
 }else{
--- PHP/dynamics/forms.php
+++ PHP/dynamics/forms.php
@@ -1,21 +1,21 @@
 <?php require 'header.php'; ?>
 
-<main>
+<main class="forms">
   <h2>Forms</h2>
   <div class="row">
     <div class="form">
-      <form class="" action="form_hander.php" method="post" enctype="multipart/form-data">
-        <input type="text" name="filename" placeholder="Filename" required>
-        <input type="file" name="file" required>
+      <form class="" action="form_handler.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="newfilename" placeholder="Filename" required>
+        <input type="file" name="upload_file" accept="image/jpeg" required>
         <input type="submit" name="submit_1" value="Upload">
       </form>
     </div>
 
     <div class="form">
-      <form class="" action="form_hander.php" method="post">
+      <form class="" action="form_handler.php" method="post">
         <input type="text" name="" placeholder="Name">
-        <input type="email" name="" placeholder="email">
-        <textarea name="name" rows="8" cols="80"></textarea>
+        <input type="email" name="" placeholder="email" required>
+        <textarea name="name" rows="8" cols="80" required></textarea>
         <input type="submit" name="submit_2" value="Send Comment">
       </form>
     </div>
